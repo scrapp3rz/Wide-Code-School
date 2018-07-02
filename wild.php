a7d0ab21bb2f2279b11f59c541d6aa9b688d7f32
Caroline Fayolle <c_fayolle@live.fr>
2018-07-02 14:39:40 +0200

    Add files via upload

---
+++ wild.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *  Repository > GIT
+ *  Q2 : Git - Les Fondamentaux
+ *  Auteur : Laurent Echeverria
+ *  Licence : Aucune
+ *  Création : 06.2018
+ */
+
+  echo "A la Wild Code School on code en chaussettes !";
+
+?>
